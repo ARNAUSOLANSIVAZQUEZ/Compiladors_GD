8277d097e9dfd527792f550272f4fe52fd754dbb
MAranich <114926980+MAranich@users.noreply.github.com>
2024-01-10 10:19:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 
 
+/*
+    Escriviu aquí els vostres noms, U i NIA per si acas: 
+
+
+
+    Marcel Aranich, U185166, NIA: 251453
+
+
+*/
+
 int main(int argc, char** argv) {
 
     printf("Test"); 
