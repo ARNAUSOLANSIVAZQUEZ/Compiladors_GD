84614c3c370ca76ca2c37aebdd9b23b7c3a8a80a
MAranich <114926980+MAranich@users.noreply.github.com>
2024-01-10 10:25:20 +0100

    argc check

--- main.c
+++ main.c
@@ -16,5 +16,12 @@ int main(int argc, char** argv) {
 
     printf("Test"); 
 
+    // argv[1] is the file to pre-process
+
+    if(argc < 2) {
+        printf("To few arguments. (incude file to preprocess)"); 
+        // TODO: print usage
+        return 1; 
+    }
 
 }
