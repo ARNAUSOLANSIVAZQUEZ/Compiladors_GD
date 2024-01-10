ade684b3d7d341e8a21221de735c2a2b176db90a
MAranich <114926980+MAranich@users.noreply.github.com>
2024-01-10 10:46:18 +0100

    base script for main

--- main.c
+++ main.c
@@ -19,9 +19,29 @@ int main(int argc, char** argv) {
     // argv[1] is the file to pre-process
 
     if(argc < 2) {
-        printf("To few arguments. (incude file to preprocess)"); 
-        // TODO: print usage
-        return 1; 
+        printf("ERROR: To few arguments. (incude file to preprocess)"); 
+        printf("Usage: %s ./{name of the program to pre-process} -{flags}", argv[0]); 
+        return 1; // return error
     }
+    
+
+    FILE* source_file = fopen(argv[1], 'r'); // we cannot modify original file
+
+    if(source_file == NULL) {
+        printf("ERROR: Could not open file: %s", argv[1]); 
+    }
+
+    // TODO: copy contents of file in main memory. 
+
+    int file_length_bytes = -1; //TODO: 
+    char* file_contents = (char*)malloc(file_length_bytes); 
+
+    //copy contents
+
+    //call every function to preprocess: 
+
+
+    free(file_contents); 
+    fclose(source_file); 
 
 }
