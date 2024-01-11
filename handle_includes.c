d846377df9fa2fe5ea59e8047168a7d82e8b557d
MAranich <114926980+MAranich@users.noreply.github.com>
2024-01-11 10:14:24 +0100

    Basic handle includes

---
+++ handle_includes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+char* handle_include_program_files(char* source_code, size_t* size_source_code) {
+    return source_code; 
+}
+
+char* handle_include_compiler_files(char* source_code, size_t* size_source_code){
+    return source_code; 
+}
+
+
+
+
+
