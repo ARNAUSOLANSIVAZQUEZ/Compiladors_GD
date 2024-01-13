e2e91ff24d4b3baebb9284fa321aad02d6200200
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-01-13 13:11:14 +0100

    Update handle_includes.c

--- Code/handle_includes.c
+++ Code/handle_includes.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 
-char* handle_define(char* source_code) {
-    
-
-    return source_code;
-}
-
-char* handle_ifdef_endif(char* source_code) {
-
-    return source_code;
-}
 
 char* handle_include_program_files(char* source_code, size_t* size_source_code) {
     // TODO: implement handle_include_program_files()
