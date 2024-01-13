75dcffc6118b7b7c29c66fe50837ce6560ab8a4a
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-01-13 14:23:26 +0100

    Update handle_ifdef_endif.h

--- Code/handle_ifdef_endif.h
+++ Code/handle_ifdef_endif.h
@@ -1,20 +1,8 @@
-#ifndef HEADER_HANDLE_BACKSLASH
-#define HEADER_HANDLE_BACKSLASH
+#ifndef HEADER_HANDLE_IFDEF_ENDIF
+#define HEADER_HANDLE_IFDEF_ENDIF
 
 #include <stdio.h>
 
-
-/*
-Handles the backslash in the code. handle_backslash() should return the an equivalent code 
-of source_code without using the "\ "
-
-
-source code is the string of the code to be pre-processed. And size_source_code is the 
-length of source_code. handle_macros() is now the owner (and responsable) of source_code. 
-
-Returns a (possibly new) string and size_source_code is updated to the new size. 
-
-*/
 
 char* handle_define(char* source_code);
 char* handle_ifdef_endif(char* source_code);
