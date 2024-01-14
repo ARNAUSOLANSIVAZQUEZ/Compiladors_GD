f2001f864e1a14dad84c4c17a2d6f3ae6a8b9ca2
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-01-14 13:36:38 +0100

    Update handle_ifdef_endif.h

--- Code/handle_ifdef_endif.h
+++ Code/handle_ifdef_endif.h
@@ -2,7 +2,16 @@
 #define HEADER_HANDLE_IFDEF_ENDIF
 
 #include <stdio.h>
+/*
+Handles the structure ifdef-endif in the code. handle_ifdef_endif() should return the text without structure ifdef_endif
+which doesn't have the string #define boolean.
 
+source code is the string of the code to be pre-processed. And size_source_code is the 
+length of source_code. handle_macros() is now the owner (and responsible) of source_code. 
+
+Returns a (possibly new) text and size_source_code is updated to the new size. 
+
+*/
 
 char* handle_ifdef_endif(char* source_code, size_t* size_source_code);
 
