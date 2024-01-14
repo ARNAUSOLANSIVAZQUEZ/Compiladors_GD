42af1a596ad1d39ddf091cc0ba14453cee3d1896
ARNAUSOLANSIVAZQUEZ <114942175+ARNAUSOLANSIVAZQUEZ@users.noreply.github.com>
2024-01-14 18:35:01 +0100

    Added fetch_directory to handle_includes.h

--- Code/handle_includes.h
+++ Code/handle_includes.h
@@ -4,6 +4,17 @@
 
 #include <stdio.h>
 
+
+/*
+ Allows to open and search directories searching for a file.
+ directory_path is a string input that represents the path to the directory in 
+ which the function will search for entries and subdirectories.
+
+ Returns the path to desired file.
+ */
+void fetch_directory(char* directory_path);
+
+
 /*
 Handles the include program files of the source code. source code is the string 
 of the code to be pre-processed. And size_source_code is the length of source_code. 
@@ -24,29 +35,6 @@ Returns a (possibly new) string and size_source_code is updated to the new size.
 
 */
 char* handle_include_compiler_files(char* source_code, size_t* size_source_code); 
-
-
-/*
-Handles the ifdef-endif of the source code. source code is the string 
-of the code to be pre-processed. And size_source_code is the length of source_code. 
-handle_ifdef_endif() is now the owner (and responsible) of source_code. 
-
-Returns a (possibly new) string and size_source_code is updated to the new size. 
-
-*/
-char* handle_ifdef_endif(char* source_code, size_t* size_source_code);
-
-/*
-Handles the define of the source code. source code is the string 
-of the code to be pre-processed. And size_source_code is the length of source_code. 
-handle_define() is now the owner (and responsible) of source_code. 
-
-Returns a (possibly new) string and size_source_code is updated to the new size. 
-
-*/
-char* handle_define(char* source_code, size_t* size_source_code);
-
-
 
 
 
