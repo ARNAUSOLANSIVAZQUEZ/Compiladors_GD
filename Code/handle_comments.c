bd1cfbfa1d20fdb556b06d609bc89af0d47f72b4
Jorge Villarino <72575622+Kata01@users.noreply.github.com>
2024-01-21 20:31:30 +0100

    Update handle_comments.c

--- Code/handle_comments.c
+++ Code/handle_comments.c
@@ -1,18 +1,62 @@
-#include <stdio.h>
+#include "handle_comments.h"
 
-
-
-char* remove_single_line_comments(char* source_code, size_t* size_source_code) {
-    // TODO: implement remove_single_line_comments()
-    return source_code; 
+// Helper function to find the index of the next newline character '\n'
+size_t find_next_newline(char* str, size_t start) {
+    size_t index = start;
+    while (str[index] != '\0' && str[index] != '\n') {
+        ++index;
+    }
+    return index;
 }
 
-char* remove_multi_line_comments(char* source_code, size_t* size_source_code) {
-    // TODO: implement remove_multi_line_comments()
-    return source_code; 
+char* handle_comments_simple(char* source_code, size_t* size_source_code) {
+    size_t read_index = 0;
+    size_t write_index = 0;
+
+    while (source_code[read_index] != '\0') {
+        if (source_code[read_index] == '/' && source_code[read_index + 1] == '/') {
+            // Found the beginning of a single-line comment, skip until the next newline
+            read_index = find_next_newline(source_code, read_index);
+        } else {
+            // Copy character to the new buffer
+            source_code[write_index] = source_code[read_index];
+            ++write_index;
+        }
+
+        ++read_index;
+    }
+
+    source_code[write_index] = '\0'; // Null-terminate the new string
+    *size_source_code = write_index; // Update the new size
+
+    return source_code;
 }
 
+char* handle_comments_multi(char* source_code, size_t* size_source_code) {
+    size_t read_index = 0;
+    size_t write_index = 0;
 
+    while (source_code[read_index] != '\0') {
+        if (source_code[read_index] == '/' && source_code[read_index + 1] == '*') {
+            // Found the beginning of a multi-line comment, skip until the comment ends
+            while (!(source_code[read_index] == '*' && source_code[read_index + 1] == '/')) {
+                ++read_index;
+            }
+            read_index += 2; // Skip the closing '*/'
+        } else {
+            // Copy character to the new buffer
+            source_code[write_index] = source_code[read_index];
+            ++write_index;
+        }
+
+        ++read_index;
+    }
+
+    source_code[write_index] = '\0'; // Null-terminate the new string
+    *size_source_code = write_index; // Update the new size
+
+    return source_code;
+}
 
 
 
