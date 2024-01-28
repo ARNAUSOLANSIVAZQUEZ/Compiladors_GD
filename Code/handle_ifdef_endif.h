62fa808747b2a6f4c343c09694965df4a9a52485
ARNAUSOLANSIVAZQUEZ <arnau.solans01@estudiant.upf.edu>
2024-01-28 22:08:24 +0100

    Fix missing bools

--- Code/handle_ifdef_endif.h
+++ Code/handle_ifdef_endif.h
@@ -26,6 +26,6 @@ length of source_code. handle_macros() is now the owner (and responsible) of sou
 Returns a (possibly new) text and size_source_code is updated to the new size. 
 
 */
-char *handle_ifdef_endif(char *source_code, int index, int *len, MultiString *ms);
+char *handle_ifdef_endif(char *source_code, int index, int *len, MultiString *ms, bool process_comments, bool process_directives);
 void pre_handle_ifdef_endif(char* reading_buffer, int i, char* writing_buffer, char* writing_buffer_len, char* writing_index, MultiString* ms, bool process_comments, bool process_directives);
 #endif
