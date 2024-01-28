a7d908f759f6f565ccf22f68211a378cfc9da326
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-01-28 23:00:26 +0100

    Update handle_backslash.c

--- Code/handle_backslash.c
+++ Code/handle_backslash.c
@@ -7,19 +7,14 @@
 *
 * Responsible: Ariadna Prat
 */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_LINE_LENGTH 256
 #include "handle_backslash.h"
-
 char* handle_backslash(char* source_code, size_t* size_source_code, int i) {
     char *result = (char*)malloc(MAX_LINE_LENGTH * sizeof(char));
     if (result == NULL) {
         fprintf(stderr, "Error: Memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
-    
+
     size_t source_length = strlen(source_code);
     size_t result_length = 0;
 
@@ -41,5 +36,19 @@ char* handle_backslash(char* source_code, size_t* size_source_code, int i) {
 
     *size_source_code = result_length; // Update the size of the processed code
     return result;
+}
+void pre_handle_backslash(char* reading_buffer, int i, char* writing_buffer, int* writing_buffer_len, int* writing_index){
+    int len = -1;
+    char *if_def_text = handle_backslash (reading_buffer, &len,i);
+    //^should return direcly what needs to be inserted in the writing buffer
+
+    if(writing_buffer_len <= writing_index + len + 1 ) { // +1 for /0
+        // get more space
+        writing_buffer_len = (int)writing_buffer_len * ARRAY_GROWTH_FACTOR;
+        writing_buffer = realloc(writing_buffer, writing_buffer_len);
+    }
+
+    memcpy(&writing_buffer[(int)writing_index - 5], if_def_text, (size_t)len);
+    writing_index += -5 + len - 1;
 }
 
