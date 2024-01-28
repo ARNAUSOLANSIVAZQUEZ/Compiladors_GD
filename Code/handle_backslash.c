ca45d76cf15bac72eb778ca64040165bd679ef0f
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-01-28 23:31:53 +0100

    Update handle_backslash.c

--- Code/handle_backslash.c
+++ Code/handle_backslash.c
@@ -37,7 +37,7 @@ char* handle_backslash(char* source_code, size_t* size_source_code, int i) {
     *size_source_code = result_length; // Update the size of the processed code
     return result;
 }
-void pre_handle_backslash(char* reading_buffer, int i, char* writing_buffer, int* writing_buffer_len, int* writing_index){
+void pre_handle_backslash(char* reading_buffer, int i,  char* writing_buffer, int* writing_buffer_len, int* writing_index){
     int len = -1;
     char *if_def_text = handle_backslash (reading_buffer, &len,i);
     //^should return direcly what needs to be inserted in the writing buffer
@@ -47,7 +47,11 @@ void pre_handle_backslash(char* reading_buffer, int i, char* writing_buffer, int
         writing_buffer_len = (int)writing_buffer_len * ARRAY_GROWTH_FACTOR;
         writing_buffer = realloc(writing_buffer, writing_buffer_len);
     }
-
+    if(*writing_buffer_len <= *writing_index + len + 1 ) { // +1 for /0
+        // get more space
+        *writing_buffer_len = *writing_buffer_len * ARRAY_GROWTH_FACTOR;
+        *writing_buffer = realloc(*writing_buffer, *writing_buffer_len);
+    }
     memcpy(&writing_buffer[(int)writing_index - 5], if_def_text, (size_t)len);
     writing_index += -5 + len - 1;
 }
